b08f96e7916a479e68072310e228131583ef9d6f
FernandoMos92 <fernandomosp@gmail.com>
2021-11-19 17:55:47 -0300

    feat: Fourth task completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,62 @@
 import React, { Component } from 'react';
 import { RiShoppingCartLine } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
+import '../Style/home.css';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+
+    this.handleCategory = this.handleCategory.bind(this);
+  }
+
+  componentDidMount() {
+    this.handleCategory();
+  }
+
+  async handleCategory() {
+    this.setState({ categories: await getCategories() });
+  }
+
   render() {
+    const { categories } = this.state;
+    console.log(categories);
     return (
-      <div>
-        <div>
-          <input type="text" placeholder="" />
-          <input type="submit" value="Search" />
+      <>
+        <header>
+          <div className="container-search">
+            <input className="input-search" type="text" placeholder="" />
+            <input className="btn-search" type="submit" value="Search" />
+          </div>
           <Link data-testid="shopping-cart-button" to="/shopping-cart">
             <RiShoppingCartLine />
           </Link>
-        </div>
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
-      </div>
+        </header>
+
+        <main>
+          <aside>
+            <ul>
+              {categories.map(({ id, name }) => (
+                <li key={ id }>
+                  <label data-testid="category" htmlFor={ id }>
+                    <input type="radio" name="categories" id={ id } />
+                    {name}
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </aside>
+          <div>
+            <p data-testid="home-initial-message">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </p>
+          </div>
+        </main>
+      </>
     );
   }
 }
